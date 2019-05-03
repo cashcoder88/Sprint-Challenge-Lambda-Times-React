9ca431403c1328b94a4d092961818fd7ffe1a934
cashcoder88 <cashglobe@gmail.com>
2019-05-03 10:43:48 -0600

    Finished functionality, need to finish prop types and styled comp

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,7 +7,7 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {props.tabs.map((tab, i) => <Tab key={i} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={props.tabs} /> )}
+        {props.tabs.map((tab, i) => <Tab key={i} tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab}  /> )}
 
       </div>
     </div>
