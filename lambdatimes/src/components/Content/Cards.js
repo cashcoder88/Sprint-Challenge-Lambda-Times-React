d32bedb9ec4d8859e6eda811eddd3ecd81405f19
cashcoder88 <cashglobe@gmail.com>
2019-05-03 10:14:27 -0600

    fixed selectTabHandler, realized I needed if else on className in Tab

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
@@ -9,7 +10,7 @@ const Cards = props => {
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+
 
 Cards.propTypes = {
   cards: PropTypes.arrayOf(PropTypes.objects)
@@ -19,3 +20,5 @@ export default Cards;
 
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+
+          // Make sure you include prop types for all of your incoming props
