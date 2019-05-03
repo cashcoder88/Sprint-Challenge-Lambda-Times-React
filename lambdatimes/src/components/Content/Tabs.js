3f7e32f3729e596f8ace4c8c9d412cb0b0f0c44c
cashcoder88 <cashglobe@gmail.com>
2019-05-03 09:26:09 -0600

    Added tabs map function

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,12 +5,21 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, i) => <Tab key={i} tab={tab} /> )}
+
       </div>
     </div>
   );
 };
 
+
+// Tabs.propTypes = {
+ 
+//     )
+// }
+
+
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+        /* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props */
