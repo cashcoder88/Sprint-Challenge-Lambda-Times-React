26ecf394e715daba95f4e69a0a3cdfa3aedbe40a
cashcoder88 <cashglobe@gmail.com>
2019-05-03 10:16:17 -0600

    onClick needed to be back inside function, put that back in with proper argument

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -7,9 +7,9 @@ const Tab = props => {
   return (
     <div
       className={(props.tab===props.selectedTab ? 'tab active-tab' : 'tab')}
-      onClick={props.selectTabHandler(props.tab)}
-      /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+      onClick={() => {
+         props.selectTabHandler(props.tab)
+      }}
     >
       {props.tab.toUpperCase()}
     </div>
@@ -25,3 +25,6 @@ export default Tab;
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+      /* Replace this dummy click handler function with your selectTabHandler function from props
+         you'll need to pass the `tab` in as an argument to this handler. */
